a616611900e68c755a6c6d79738180b6cfcb42cd
Aderogba Samuel <121461245+Devsamzy@users.noreply.github.com>
2024-06-18 01:15:16 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log('Hello World!');
+var nav=document.querySelector('.nav');
+document.querySelector('.navbn').onclick=()=>{
+nav.classList.toggle("navs");
+}
+/*
+var firstname=document.querySelector('#firstname');
+var lastname=document.querySelector('#lastname');
+var middlename=document.querySelector('#middlename');
+var message=document.querySelector('.text');
+var age=document.querySelector('#age');
+var info=document.querySelector('.info');
+var msg='Hello the Director, I am texting to message you for the registration of my child of age'+''+age.value+''+'Who bears the name'+''+firstname.value+''+lastname.value+''+middlename.value+''+'and class ss1. I will be awaiting your response sir';
+document.querySelector('#sub').onclick=()=>{
+  info.classList='show';
+   
+  message.append(msg)
+  
+ window.href='#'
+  
+};
+
+*/
